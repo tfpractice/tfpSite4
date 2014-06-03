ae8a3b13f7beef0410a44ce4ae9ec8dd44cb7527
theory friction practice <jackson.brebnor@gmail.com>
2014-06-03 19:19:20 -0400

    places content onto portfolio pages with cunty partials

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,44 @@
 class PortfolioController < ApplicationController
-  def index
-  end
+	  before_action :set_subject, :set_project, :set_category
+
+
+   def index
+  	@subjects = Subject.includes(:projects, :categories).all
+  	@projects = Project.includes(:categories).all
+  	@categories = Category.includes(:projects).all
+  	#@subject = Subject.find(params[:id])
+     
+     respond_to do |format|
+      format.html
+      format.xml { render :xml => @projects.to_xml }
+      format.js {}
+    end
+
+end
+
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_subject
+     # @subject = Subject.find(params[:id])
+      @subject = Subject.includes(:projects, :categories).where(id: params[:id])
+
+    end
+    def set_project
+      @project = Project.includes(:categories).where(id: params[:id])
+    end
+    def set_category
+      @category = Category.includes(:projects).where(id: params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def subject_params
+      params.require(:subject).permit(:name, :description, :portfolio)
+    end
+    def category_params
+      params.require(:category).permit(:name, :description, :portfolio)
+    end
+    def project_params
+      params.require(:project).permit(:name, :description, :portfolio, :year, :subject_id, :subject_type)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'portfolio/show'
+
   namespace :admin do
     resources :skills
   end
