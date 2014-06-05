176e79f3d90f59e9e6121fe945f4d73f0e098dfb
theory friction practice <jackson.brebnor@gmail.com>
2014-06-05 15:04:43 -0400

    gets these little divs set up for my development work yasss (couple speedbumps on the erb front but we aiight

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ActiveRecord::Base
   #belongs_to :category, polymorphic: true
   belongs_to :skillable, polymorphic: true
 
-
+  scope :svg, -> {where(worktype: "svg")}
   scope :styling, -> {where(worktype: "styling")}
   scope :scripting, -> {where(worktype: "scripting")}
   scope :framework, -> {where(worktype: "programming languages and frameworks")}
