d0352805713694b6395609fb5e8289d55232d785
theory friction practice <jackson.brebnor@gmail.com>
2014-06-09 16:08:16 -0400

    GETS THEM RESPONSIVE ARCS

--- app/assets/javascripts/d3Portfolio.js
+++ app/assets/javascripts/d3Portfolio.js
@@ -129,12 +129,65 @@ $(document).ready(function (){
 
 });
 
- d3.selectAll(".skillsDiv").append("p").text("is this thing on?"); 
+var skillData2 = [1,12,33];
+
+var svgWidth = $(".skillsDiv").width();
+var svgHeight = $(".skillsDiv").height();
+var iRad = 125;
+var oRad = 150;
+var fullCircle = Math.PI * 2;
+
+var color = d3.scale.ordinal()
+				.range(["red", "white", "blue"]);
+
+var skillCanvases = d3.selectAll(".skillsDiv").append("svg")
+ 						.attr("width", "100%")
+ 						.attr("height", svgWidth)
+ 						.attr("viewBox", "0,0,500,500"); 
+
+var group = skillCanvases.append("g")
+				.attr("transform", "translate (250, 250)");
+
+var arc = d3.svg.arc()
+			.innerRadius(iRad)
+			.outerRadius(oRad)
+			//.startAngle(0)
+//			.endAngle(fullCircle);
+
+var graph = d3.layout.pie()
+			.value(function(d) {
+
+				return d;
+			});			
+
+
+
+var arcs = group.selectAll(".arc")
+			.data(graph(skillData2))
+			.enter()
+				.append("g")
+				.attr("class", "arc");			
+
+
+arcs.append("path")
+	.attr("d", arc)
+	.attr("fill", function(d){ return color(d*20); });
+
+
+//group.append("path")
+//	.attr("d", arc)
+//	.attr("fill", "#ffffff");
+
+
+
+
+d3.json("skills.json", function(data) {
 
 
 
 
 
+});
 
 
 
